b48d0e48166bdb669b8a9f4f9e20864e3440bfe7
Karan Raj <kr74747@gmail.com>
2025-04-25 00:57:56 +0530

    Applied changes in file

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import numpy as np
 app = FastAPI()
 
 # Load the trained model
-with open("model.pkl", "rb") as f:
+with open("app/model.pkl", "rb") as f:
     model = pickle.load(f)
 
 class StockData(BaseModel):
